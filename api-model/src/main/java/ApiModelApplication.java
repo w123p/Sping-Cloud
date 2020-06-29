bdfd488a2cc3869f8897b3ccdb1e32032d61fd73
weiping <1508195748@qq.com>
2020-06-29 10:07:25 +0800

    测试

---
+++ api-model/src/main/java/ApiModelApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.model;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiModelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiModelApplication.class, args);
+    }
+
+}
---
+++ oauth-center/src/main/java/com/cloud/oauth/config/PasswordEncoderConfig.java
@@ -0,0 +1,20 @@
+package com.cloud.oauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * 密码校验器<br>
+ * 2018.08.01
+ *
+ * @author weiping wp18276225616@163.com
+ */
+@Configuration
+public class PasswordEncoderConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
